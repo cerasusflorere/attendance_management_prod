f89add70f2508342c581254efb9d50ea70ff6c33
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2021-10-29 08:11:11 +0900

    Create index.php
    トップ画面

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
+    <link rel="icon" href="img_news_00.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class='login-area'>
+        <div class='login-page'>
+            <div class='login-form'>
+                <h3 class='login-title'>小早川・早見研究室　来校ログ</h3>
+                <div class='login-botton-area'>
+                    <a class='login-button' href='management.php'><i class="fas fa-eye fa-fw"></i>閲覧</a>
+                    <a class='login-button' href='attendance.php'><i class="fas fa-pen-square fa-fw"></i>登録</a>
+                </div>                
+            </div>
+        </div>
+        
+    </div>
+ 
+</body>
+</html>
