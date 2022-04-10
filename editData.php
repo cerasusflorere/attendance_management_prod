bfd25073678535511337060bfab1803370ab942d
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2022-04-10 23:45:54 +0900

    Create editData.php

---
+++ editData.php
@@ -0,0 +1,34 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<?php
+    session_start();
+    header('Content-type: application/json; charset=UTF-8');
+    
+    $json = file_get_contents('php://input');
+    $log_origin = json_decode($json, true);
+    
+    $mysqli = new mysqli('***, '***', '***', '***');
+    if($mysqli->connect_error){
+        echo $mysqli->connect_error;
+        exit();
+    } else {
+        $mysqli->set_charset("utf8mb4");
+    }
+     // ここでデータベースを更新する
+     try{
+        $stmt = $mysqli -> prepare("UPDATE member SET `name`=?, `position`=?, `studentID`=?, `year`=? WHERE `id`=?");
+        $stmt -> bind_param('ssiii', $log_origin['name'], $log_origin['position'], $log_origin['studentID'], $log_origin['year'], $log_origin['id']);
+        $stmt -> execute();
+        $_SESSION = array();
+    }catch(PDOException $e){
+       //トランザクション取り消し
+        $pdo -> rollBack();
+        $errors['error'] = "もう一度やり直してください。";
+        print('Error:'.$e->getMessage());
+    }
